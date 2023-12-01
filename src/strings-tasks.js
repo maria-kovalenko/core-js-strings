a508fe5e92c0414337f20d24d23e10201c3fc4be
Maria <workmail358@gmail.com>
2023-12-01 22:52:35 +0300

    feat: solve task sumOfCodes

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -184,8 +184,15 @@ function removeLastOccurrences(/* str, value */) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  let sum = 0;
+  if (str === undefined || str === null) {
+    return 0;
+  }
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str[i].charCodeAt(0);
+  }
+  return sum;
 }
 
 /**
